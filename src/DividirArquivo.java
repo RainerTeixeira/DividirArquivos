4ba1bc93976a655a86fbf65086c6d7a478f72bc7
Rainer Teixeira <raineroliveira94@hotmail.com>
2023-04-13 12:15:38 -0300

    Dividir arquivos em partes ...

---
+++ src/DividirArquivo.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import javax.swing.*;
+
+public class DividirArquivo {
+
+    public static void main(String[] args) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.showOpenDialog(null);
+
+        File arquivoOriginal = fileChooser.getSelectedFile();
+
+        // Armazenando a primeira e a última linha do arquivo original
+        String primeiraLinha = null;
+        String ultimaLinha = null;
+        try (BufferedReader br = new BufferedReader(new FileReader(arquivoOriginal))) {
+            primeiraLinha = br.readLine(); // Lê a primeira linha
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                ultimaLinha = linha;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        int numPartes = Integer.parseInt(JOptionPane.showInputDialog("Quantas partes deseja dividir o arquivo?"));
+
+        int numLinhas = contarLinhas(arquivoOriginal);
+        int linhasPorParte = numLinhas / numPartes;
+        int linhasRestantes = numLinhas % numPartes;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(arquivoOriginal))) {
+            br.readLine(); // Ignora a primeira linha
+            for (int i = 1; i <= numPartes; i++) {
+                String nomeArquivo = arquivoOriginal.getName() + ".part" + i;
+                try (BufferedWriter bw = new BufferedWriter(new FileWriter(nomeArquivo))) {
+
+                    // Escrevendo a primeira linha em todos os arquivos de parte
+                    bw.write(primeiraLinha);
+                    bw.newLine();
+
+                    // Escrevendo as linhas da parte atual, exceto a primeira linha (já escrita acima)
+                    int linhasEscritas = 0;
+                    while (linhasEscritas < linhasPorParte) {
+                        String linha = br.readLine();
+                        if (linha == null) {
+                            break;
+                        }
+                        bw.write(linha);
+                        bw.newLine();
+                        linhasEscritas++;
+                    }
+
+                    // Se ainda há linhas restantes e estamos na última parte, escrevê-las aqui
+                    if (i == numPartes && linhasRestantes > 0) {
+                        for (int j = 0; j < linhasRestantes; j++) {
+                            String linha = br.readLine();
+                            if (linha == null) {
+                                break;
+                            }
+                            bw.write(linha);
+                            bw.newLine();
+                        }
+                    }
+
+                    // Escrevendo a última linha no final de cada arquivo de parte, exceto na última parte
+                    if (i < numPartes && ultimaLinha != null) {
+                        bw.write(ultimaLinha);
+                        bw.newLine();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int contarLinhas(File arquivo) {
+        int numLinhas = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
+            while (br.readLine() != null) {
+                numLinhas++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1;
+        }
+        return numLinhas;
+    }
+}
